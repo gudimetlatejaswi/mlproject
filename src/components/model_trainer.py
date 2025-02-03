636d3cc974249a6cad79568f783289f7f710bc93
Tejaswi-gudimetla <vgudimet@buffalo.edu>
2025-02-03 16:50:12 -0500

    Hyper Parameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -46,45 +46,19 @@ class ModelTrainer:
                 "CatBoosting Classifier": CatBoostRegressor(verbose=False),
                 "AdaBoost Classifier": AdaBoostRegressor(),
             }
-            params={
-                "Decision Tree": {
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    # 'splitter':['best','random'],
-                    # 'max_features':['sqrt','log2'],
-                },
-                "Random Forest":{
-                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                 
-                    # 'max_features':['sqrt','log2',None],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "Gradient Boosting":{
-                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                    'learning_rate':[.1,.01,.05,.001],
-                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    # 'criterion':['squared_error', 'friedman_mse'],
-                    # 'max_features':['auto','sqrt','log2'],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "Linear Regression":{},
-                "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "CatBoosting Regressor":{
-                    'depth': [6,8,10],
-                    'learning_rate': [0.01, 0.05, 0.1],
-                    'iterations': [30, 50, 100]
-                },
-                "AdaBoost Regressor":{
-                    'learning_rate':[.1,.01,0.5,.001],
-                    # 'loss':['linear','square','exponential'],
-                    'n_estimators': [8,16,32,64,128,256]
-                }
-                
+            params = {
+                'Random Forest': {'n_estimators': [50, 100, 200], 'max_depth': [None, 10, 20]},
+                'Decision Tree': {'max_depth': [3, 5, 10], 'min_samples_split': [2, 5, 10]},
+                'Gradient Boosting': {'n_estimators': [50, 100], 'learning_rate': [0.01, 0.1, 0.2]},
+                'Linear Regression': {},  
+                'K-Neighbors Classifier': {'n_neighbors': [3, 5, 7], 'weights': ['uniform', 'distance']},
+                'XGBClassifier': {'n_estimators': [50, 100], 'learning_rate': [0.01, 0.1]},
+                'CatBoosting Classifier': {'iterations': [100, 200], 'learning_rate': [0.01, 0.1]},
+                'AdaBoost Classifier': {'n_estimators': [50, 100], 'learning_rate': [0.01, 0.1]}
             }
-            
-            model_report:dict=  evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models,param=params)
+
+            model_report:dict=  evaluate_models(X_train=x_train,y_train=y_train,X_test=x_test,y_test=y_test, models=models,param=params)
+        
             
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
